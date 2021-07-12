663aa74eeaa65ba34d339a6a5b51b1edd81137cf
Francisco Ceruti <hello@fceruti.com>
2021-07-12 11:46:35 -0400

    mix new elixir-picasso --app picasso --module Picasso

---
+++ lib/picasso.ex
@@ -0,0 +1,18 @@
+defmodule Picasso do
+  @moduledoc """
+  Documentation for `Picasso`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Picasso.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/picasso_test.exs
@@ -0,0 +1,8 @@
+defmodule PicassoTest do
+  use ExUnit.Case
+  doctest Picasso
+
+  test "greets the world" do
+    assert Picasso.hello() == :world
+  end
+end
