a67ff777cd50ec829acb66ccac1b5c92a89c0dca
Francisco Ceruti <hello@fceruti.com>
2021-07-12 19:49:59 -0400

    add example dir to formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,8 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: [
+    "{mix,.formatter}.exs",
+    "{config,lib,test}/**/*.{ex,exs}",
+    "example/{config,lib,test}/**/*.{ex,exs}"
+  ]
 ]
