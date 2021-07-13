2547156f1fa209c2f4bfc19f4514c78a9f54e13c
Francisco Ceruti <hello@fceruti.com>
2021-07-12 20:51:39 -0400

    add mogrify

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Picasso.MixProject do
     [
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix, "~> 1.5.8"}
+      {:phoenix, "~> 1.5.8"},
+      {:mogrify, "~> 0.9.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
